7f1754ec3898f583239918aecd4b73a4f0db20b9
Serenya Dyadev <3684182@gmail.com>
2020-05-06 14:39:40 +0300

    js добавлена проверка на наличие класса с которым нужно работать

--- source/js/script.js
+++ source/js/script.js
@@ -6,13 +6,15 @@ var navToggleMenu = document.querySelector('.main-nav__toggle-menu');
 navToggle.classList.remove('main-nav__toggle--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navToggleCross.classList.contains('main-nav__toggle-cross--active')) {
-    navToggleCross.classList.remove('main-nav__toggle-cross--active');
-    navToggleMenu.classList.add('main-nav__toggle-menu--active');
-    navMainList.classList.add('main-nav__list--closed');
-  } else {
-      navToggleCross.classList.add('main-nav__toggle-cross--active');
-      navToggleMenu.classList.remove('main-nav__toggle-menu--active');
-      navMainList.classList.remove('main-nav__list--closed');
-    }
+  if (navToggle) {
+    if (navToggleCross.classList.contains('main-nav__toggle-cross--active')) {
+      navToggleCross.classList.remove('main-nav__toggle-cross--active');
+      navToggleMenu.classList.add('main-nav__toggle-menu--active');
+      navMainList.classList.add('main-nav__list--closed');
+    } else {
+        navToggleCross.classList.add('main-nav__toggle-cross--active');
+        navToggleMenu.classList.remove('main-nav__toggle-menu--active');
+        navMainList.classList.remove('main-nav__list--closed');
+      }
+  }
 });
